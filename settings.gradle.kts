b65be9018f8dcab07ef6ec2dc308b41462788bd2
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-29 12:21:54 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,5 @@
 pluginManagement {
 	repositories {
-		google()
-		mavenCentral()
 		gradlePluginPortal()
 	}
 }
@@ -9,10 +7,12 @@ pluginManagement {
 dependencyResolutionManagement {
 	repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
 	repositories {
-		google()
-		mavenCentral()
 		gradlePluginPortal()
 	}
+}
+
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 include(":appLab1")
